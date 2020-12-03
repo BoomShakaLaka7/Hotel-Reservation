a2fbd58d948f4221d6f627886aa65c5a5d953623
Anish-1703 <71100656+Anish-1703@users.noreply.github.com>
2020-12-03 10:03:03 -0800

    View2 Complete.
    Added a text field and Reserve Button. the text field will take the ask the user to enter the desired hotel id and the reserve button will take them to view3 and also close view2 once the user moves to view3

--- Hotel/src/com/company/view/View2.java
+++ Hotel/src/com/company/view/View2.java
@@ -4,6 +4,10 @@ import com.company.model.Hotel;
 import com.company.model.Model;
 
 import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -12,11 +16,15 @@ import java.util.List;
 public class View2 {
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
-//    Model model;
+    //    Model model;
 //    View view = new View();
     JTextArea results = new JTextArea(20, 6);
     List<Hotel> hotelList = new ArrayList<>();
     int hotelsFound = 0;
+    JButton reserveButton = new JButton("Reserve");
+    //JTextArea selectedFieldName = new JTextArea();
+
+    JTextField enteredValue = new JTextField("Enter Selected Hotel ID");
 
     public View2(Model model, View view) {
         //            results.setBounds(10, 10, 400, 350);
@@ -28,6 +36,26 @@ public class View2 {
 
         results.setEditable(false);
         results.setText("");
+
+        reserveButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                // TODO Auto-generated method stub
+                for(int i = 0; i<hotelsFound; i++) {
+                    if(Integer.parseInt(enteredValue.getText()) == i) {
+                        View3 view3 = new View3();
+                    frame.setVisible(false);
+                    }
+                }
+            }
+
+
+        }
+
+        );
+
         String city = view.getCity();
         Date checkIn = view.getCheckIn();
         Date checkOut = view.getCheckOut();
@@ -87,7 +115,12 @@ public class View2 {
         }
 
         panel.add(results);
+        //panel.add(selectedFieldName);
+        panel.add(enteredValue);
+        panel.add(reserveButton);
         frame.add(panel);
+
+
 //            panel.setLayout(null);
     }
 
